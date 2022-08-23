c8fe71b3477f0f9238cecddf076843969458f369
unknown <dimasfadillah05@gmail.com>
2022-08-23 10:57:31 +0700

    Upload Ke Github

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('layout.app');
+});
+
+Route::get('/barang', function () {
+    return view('barang.index');
+});
+
+Route::get('/kategori', function () {
+    return view('kategori.index');
+});
+
+Route::get('/suplier', function () {
+    return view('suplier.index');
+});
+
+Route::get('/pembelian', function () {
+    return view('pembelian.index');
+});
+
+Route::get('/penjualan', function () {
+    return view('penjualan.index');
+});
+
